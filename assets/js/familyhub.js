51c17fdbe89f78beeeda1feac006db2368932d8d
AGreaves <gilhespy.anna@gmail.com>
2019-05-28 09:46:17 +0000

    add floating button to return to top of screen

---
+++ assets/js/familyhub.js
@@ -0,0 +1,17 @@
+
+/* CREDIT: code for floating buttons taken from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("to-top-btn").style.display = "block";
+  } else {
+    document.getElementById("to-top-btn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
