71beff534fe6c304d74e230b951927e22e4097c8
AGreaves <gilhespy.anna@gmail.com>
2019-05-28 12:13:09 +0000

    add JS to open and close the search modal

--- assets/js/familyhub.js
+++ assets/js/familyhub.js
@@ -1,5 +1,5 @@
 
-/* CREDIT: code for floating buttons taken from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp */
+// CREDIT: code for floating buttons taken from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -17,3 +17,11 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+function openSearch() {
+    document.getElementById("search-modal").style.display = "block";
+}
+
+function clickSearch() {
+    document.getElementById("search-modal").style.display = "none";
+}
