7051dfd71245d7f88be0105d69f8197776817728
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-02 20:25:42 +0200

    create first renderd page with app.py, move templates folder to root

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, request
 
 # create instance of flask and assign it to "app"
 app = Flask(__name__) 
 
 # create test function as proof of concept
 @app.route('/')
-def hello():
-    return 'Hello World... again!'
+def home_page():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=False)
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
