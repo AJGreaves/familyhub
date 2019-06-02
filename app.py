2a9f7a40ebf1773f873990f7ce012031ad4af154
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-02 12:23:15 +0200

    update file name images to img to fit with flask requirements

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 # create instance of flask and assign it to "app"
@@ -8,3 +9,7 @@ app = Flask(__name__)
 def hello():
     return 'Hello World... again!'
 
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
