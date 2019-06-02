f8522e2898b30eb7ac331255d826b51757067e72
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-02 15:14:51 +0200

    adjust host and port code to run on local rather than old c9 version

--- app.py
+++ app.py
@@ -10,6 +10,4 @@ def hello():
     return 'Hello World... again!'
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
