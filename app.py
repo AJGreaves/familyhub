6997e2a110479a0acf49adfa23a666c31406ced0
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-02 21:43:56 +0200

    create routes for actitivies, contact and events in app.py

--- app.py
+++ app.py
@@ -4,10 +4,25 @@ from flask import Flask, render_template, redirect, request, url_for, request
 # create instance of flask and assign it to "app"
 app = Flask(__name__) 
 
-# create test function as proof of concept
+# Home page
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", title="Home", active="home")
+
+# Activities page
+@app.route('/activities')
+def activities_page():
+    return render_template("activities.html", title="Activities", active="activities")
+
+# Events page
+@app.route('/events')
+def events_page():
+    return render_template("events.html", title="Events", active="events")
+
+# Contact page
+@app.route('/contact')
+def events_page():
+    return render_template("contact.html", title="Contact", active="contact")
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
