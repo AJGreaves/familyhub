fe9d96d7a91e7cacfb6d876520b54beb7e0f0b40
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-03 14:04:30 +0200

    keys.py file not connecting to app.py correctly. new branch created to store fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
+from app.keys import Keywords
 
 # create instance of flask and assign it to "app"
 app = Flask(__name__) 
@@ -7,67 +8,95 @@ app = Flask(__name__)
 # Home page
 @app.route('/')
 def home_page():
-    return render_template("index.html", title="Home", active="home")
+    return render_template("index.html", 
+                            title="Home", 
+                            active="home",
+                            keywords=Keywords.home)
 
 # Activities page
 @app.route('/activities')
 def activities_page():
-    return render_template("activities.html", title="Activities", active="activities")
+    return render_template("activities.html", 
+                            title="Activities", 
+                            active="activities",
+                            keywords=Keywords.activities)
 
 # Events page
 @app.route('/events')
 def events_page():
-    return render_template("events.html", title="Events", active="events")
+    return render_template("events.html",
+                            title="Events", 
+                            active="events",
+                            keywords=Keywords.events)
 
 # Contact page
 @app.route('/contact')
 def contact_page():
-    return render_template("contact.html", title="Contact", active="contact")
+    return render_template("contact.html", 
+                            title="Contact", 
+                            active="contact")
 
 # Create Account page
 @app.route('/newaccount')
 def new_account_page():
-    return render_template("newaccount.html", title="Create Account", active="newAccount")
+    return render_template("newaccount.html", 
+                            title="Create Account", 
+                            active="newAccount")
 
 # Contact page
 @app.route('/login')
 def login_page():
-    return render_template("login.html", title="Log In", active="login")
+    return render_template("login.html", 
+                            title="Log In", 
+                            active="login")
 
 # Search page
 @app.route('/search')
 def search_page():
-    return render_template("search.html", title="Search")
+    return render_template("search.html", 
+                            title="Search")
 
 # Activity listing page - see if possible to update this to different routes based on each activity title
 @app.route('/activity-listing')
 def activity_listing_page():
-    return render_template("activitylisting.html", title="Activity Listing")
+    return render_template("activitylisting.html", 
+                            title="Activity Listing")
 
 # Event listing page - see if possible to update this to different routes based on each event title
 @app.route('/event-listing')
 def event_listing_page():
-    return render_template("eventlisting.html", title="Event Listing")
+    return render_template("eventlisting.html", 
+                            title="Event Listing")
 
 # Search page
 @app.route('/settings')
 def settings_page():
-    return render_template("settings.html", title="Account Settings", loginStatus=True)
+    return render_template("settings.html", 
+                            title="Account Settings", 
+                            loginStatus=True)
 
 # Account page - all listings for this account
 @app.route('/account')
 def my_account_page():
-    return render_template("account.html", title="My Account", loginStatus=True)
+    return render_template("account.html", 
+                            title="My Account", 
+                            loginStatus=True)
 
 # Add new event page
 @app.route('/add-new-event')
 def new_event_page():
-    return render_template("addevent.html", title="Add New Event", loginStatus=True)
+    return render_template("addevent.html", 
+                            title="Add New Event", 
+                            loginStatus=True)
 
 # Add new activity page
 @app.route('/add-new-activity')
 def new_activity_page():
-    return render_template("addactivity.html", title="Add New Activity", loginStatus=True)
+    return render_template("addactivity.html", 
+                            title="Add New Activity", 
+                            loginStatus=True)
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+    app.run(host=os.getenv('IP'), 
+            port=os.getenv('PORT'), 
+            debug=True)
---
+++ app/keys.py
@@ -0,0 +1,15 @@
+home_keywords = ['family', 'kids', 'children', 'child', 'event', 'events', 'activity', 'activities', 'things to do', 'search', 'find', 
+                'days out', 'family hub', 'familyhub', 'Aalsmeer', 'Badhoevedorp','Bloemendaal', 'Cruquius', 'Haarlem',
+                'Heemstede', 'Hillegom', 'Hoofddorp', 'Lisse', 'Nieuw-Vennep', 'Santpoort-Noord', 'Uithorn', 'Zandvoort', 'Haarlemmermeer']
+
+activities_keywords = ['activities for kids', 'kids', 'fun things to do', 'activity', 'kids sports', 'Creative kids', 
+                    'Science and Tech for kids', 'Cultural kids', 'Music for kids', 'kids Yoga', 'kids Mindfulnes', 
+                    'Museums and Exhibitions', 'Parks and playgrounds', 'in english' ]
+
+events_keywords = ['events for kids', 'kids', 'fun things to do', 'event', 'kids day out', 'family days out', 'family festival',
+                    'music', 'kids events', 'in english' ]
+
+class Keywords:
+    home = ', '.join(str(word) for word in home_keywords)
+    activities = ', '.join(str(activity) for activity in activities_keywords)
+    events = ', '.join(str(event) for event in events_keywords)
