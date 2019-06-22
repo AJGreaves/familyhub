472afa62626a8b716dd5125bafef5fa20434e4d9
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-22 21:46:47 +0200

    Add JS to remove extra "active" classes on carousel items.

--- static/js/index.js
+++ static/js/index.js
@@ -7,3 +7,10 @@
  * Credit: https://stackoverflow.com/questions/17332431/how-can-i-control-the-speed-that-bootstrap-carousel-slides-in-items/18633703 
  */
 jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000;
+
+document.addEventListener("DOMContentLoaded", function() {
+    let slides = $('.carousel-item').not(':first');
+    slides.each(function() {
+        $(this).removeClass('active');
+    })
+});
