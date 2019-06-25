d4f9c4fbeff3ce920d4c62ae4a25ee10d4c06c74
Anna Greaves <gilhespy.anna@gmail.com>
2019-06-25 09:16:35 +0200

    Create getDisplayArray function to push first 12 results from data to displayArray

--- static/js/filters.js
+++ static/js/filters.js
@@ -8,10 +8,18 @@ showLoading();
 fetch('/api/activities')
 .then(res => res.json())
 .then(data => {
-    console.log(data)
+    console.log(data);
+    getDisplayArray(data);
     hideLoading();
 })
 .catch(err => console.log(err));
 
+let displayArray = []
+function getDisplayArray(data) {
+    for (i = 0; i < 12; i++){
+        displayArray.push(data[i]);
+    }
+    console.log(displayArray);
+}
 
 
