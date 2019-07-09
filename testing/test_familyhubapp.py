d67860adfbfd45e2af9a68df0d697704dd38e5ed
Anna Greaves <gilhespy.anna@gmail.com>
2019-07-09 12:50:50 +0200

    Python testing: Tests for open_times() complete.

--- testing/test_familyhubapp.py
+++ testing/test_familyhubapp.py
@@ -31,7 +31,43 @@ def testSlugify():
 
 testSlugify()
 
+def test_open_times():
+    openTimes_db = {
+        'monStart': None, 
+        'monEnd': None, 
+        'tueStart': None, 
+        'tueEnd': None, 
+        'wedStart': datetime(1900, 1, 1, 15, 30), 
+        'wedEnd': datetime(1900, 1, 1, 17, 0), 
+        'thuStart': None, 
+        'thuEnd': None, 
+        'friStart': None, 
+        'friEnd': None, 
+        'satStart': datetime(1900, 1, 1, 10, 0), 
+        'satEnd': datetime(1900, 1, 1, 11, 30), 
+        'sunStart': datetime(1900, 1, 1, 10, 0), 
+        'sunEnd': datetime(1900, 1, 1, 16, 30)
+    }
+    openTimes = Helpers.open_times(openTimes_db)
+    expectedResult = [
+        {'monStart': None}, 
+        {'monEnd': None}, 
+        {'tueStart': None}, 
+        {'tueEnd': None}, 
+        {'wedStart': '15:30'}, 
+        {'wedEnd': '17:00'}, 
+        {'thuStart': None}, 
+        {'thuEnd': None}, 
+        {'friStart': None}, 
+        {'friEnd': None}, 
+        {'satStart': '10:00'}, 
+        {'satEnd': '11:30'}, 
+        {'sunStart': '10:00'}, 
+        {'sunEnd': '16:30'}
+    ]
+    assert openTimes == expectedResult, "open_times() should return array of objects with datetime objects converted into times to display"
 
+test_open_times()
 
 
 print("All tests passed")
