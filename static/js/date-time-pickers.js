dba6d8f2c0a9c4331423386c65e839a5017f5b26
Anna Greaves <gilhespy.anna@gmail.com>
2019-07-19 09:42:55 +0200

    Adjust comment in dat-time-pickers.js

--- static/js/date-time-pickers.js
+++ static/js/date-time-pickers.js
@@ -1,11 +1,8 @@
-/******************************************
- * JS functions for date and time pickers
- * only needed on add/edit forms and in search
- * filter pages
- ******************************************/
+/**
+ * datepicker function code written by fellow student Sean Murphy, 
+ * who gave it to me to demonstrate how to get it working
+ */
 
-// datepicker function code written by fellow student Sean Murphy, 
-// who gave it to me to demonstrate how to get it working
 ['#activityFilterDatepickerSm',
     '#activityFilterDatepickerLg',
     '#start',
@@ -21,7 +18,6 @@
         language: "it",
     });
 });
-
 
 ['#monStart', '#monEnd', '#tueStart', '#tueEnd', '#wedStart', '#wedEnd', '#thuStart',
     '#thuEnd', '#friStart', '#friEnd', '#satStart', '#satEnd', '#sunStart', '#sunEnd',
